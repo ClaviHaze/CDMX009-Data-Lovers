735ca1c6e113931c96b4fbfa56c90f5bb3aefe38
ClaviHaze <60659234+ClaviHaze@users.noreply.github.com>
2020-03-03 08:14:16 -0600

    Se añadio css y función de prueba para tabla de contenido

--- src/main.js
+++ src/main.js
@@ -2,3 +2,15 @@ import { example } from './data.js';
 
 console.log(example);
 
+/*let testarray = ["Bulbasaur", "Ivysaur", "Venasaur",
+"Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise"]
+let tr;
+testarray.forEach((pokemon, i) => {
+  let td = document.createElement("td");
+  if (!(i % 3)) {
+    tr = document.createElement("tr");
+    document.getElementById("pokeList").appendChild(tr);
+  }
+  td.appendChild(document.createTextNode(pokemon));
+  tr.appendChild(td);
+});*/
