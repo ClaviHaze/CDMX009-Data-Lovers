c1e70e8ace2451a50e655f8e2551a816d7b673ec
ClaviHaze <60659234+ClaviHaze@users.noreply.github.com>
2020-03-05 16:21:59 -0600

    se añadio barra de tipos y huevo.

--- src/data.js
+++ src/data.js
@@ -1,28 +1,8 @@
-// import data from './data/injuries/injuries.js';
-// import data from './data/lol/lol.js';
-// import data from './data/patient/patient.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/steam/steam.js';
-// import data from './data/steam/worldbank.js';
-
 //AQUÍ VAN LAS FUNCIONES DE FILTRADO
-
-// esta es una función de ejemplo
-
 fetch('https://raw.githubusercontent.com/ClaviHaze/CDMX009-Data-Lovers/master/src/data/pokemon/pokemon.json')
-
   .then(response => response.json())
   .then(json => console.log(json));
-    
 
-
-
-
-
-
-
-/*export const example = () => {
-  return 'example';
-};*/
-
+  export const example = () => {
+    return json;
+  };
--- src/main.js
+++ src/main.js
@@ -1,28 +1,33 @@
 //AQUÍ VA LA MANIPULACIÓN DEL DOM
+import {example} from './data.js'
 
+let showAll = () => {
+  document.getElementById('egg').style.display = 'none';
+  document.getElementById('types').style.display = 'none';
+  document.getElementById('datatest').innnerHTML(example);
+}
 document.getElementById('allPoke').addEventListener('click', showAll);
-function showAll() {
-  console.log("Mostrar Todos");
-}
 
+let showTopSpawns = () => {
+  document.getElementById('egg').style.display = 'none';
+  document.getElementById('types').style.display = 'none';
+}
 document.getElementById('topSpawn').addEventListener('click', showTopSpawns);
-function showTopSpawns() {
-  console.log("Top 10");
-}
 
+let showByType = () => {
+  document.getElementById('egg').style.display = 'none';
+  document.getElementById('types').style.display = 'flex';
+}
 document.getElementById('pokeType').addEventListener('click', showByType);
-function showByType() {
-  console.log("Por Tipo");
-}
 
+let showByEgg = () => {
+  document.getElementById('egg').style.display = 'flex';
+  document.getElementById('types').style.display = 'none';
+}
 document.getElementById('eggKm').addEventListener('click', showByEgg);
-function showByEgg() {
-  console.log("Por Huevo");
-}
 
+let restart = () => {
+  document.getElementById('egg').style.display = 'none';
+  document.getElementById('types').style.display = 'none';
+}
 document.getElementById('home').addEventListener('click', restart);
-function restart() {
-  console.log("Pantalla Principal");
-}
-
-document.getElementById("poke").innerHTML("Pokemon 1")
