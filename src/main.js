a737ca5bee6e9a084be9ef01f791da32a1c3e736
ClaviHaze <60659234+ClaviHaze@users.noreply.github.com>
2020-03-08 04:04:46 -0600

    Se añadio icons.js con arreglo para añadir barras de tipo y huevo con función automática. Se incluyeron las funciones nuevas en main.js y la función en data.js para el fetch y la creación de tabla

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 //AQUÍ VA LA MANIPULACIÓN DEL DOM
+import icons from './icons.js'
 import {example} from './data.js'
 
-let showAll = () => {
+const navTypes = document.querySelector('#types');
+const navEggs = document.querySelector('#egg');
+
+let showAll = (example) => {
   document.getElementById('egg').style.display = 'none';
   document.getElementById('types').style.display = 'none';
-  document.getElementById('datatest').innnerHTML(example);
 }
 document.getElementById('allPoke').addEventListener('click', showAll);
 
@@ -14,18 +17,59 @@ let showTopSpawns = () => {
 }
 document.getElementById('topSpawn').addEventListener('click', showTopSpawns);
 
-let showByType = () => {
+let showTypeList = () => {
   document.getElementById('egg').style.display = 'none';
   document.getElementById('types').style.display = 'flex';
+  navTypes.textContent = '';
+  for (let i=0; i < icons.types.length; i++) {
+    const typeIcon = document.createElement('img');
+    typeIcon.src = icons.types[i];
+    typeIcon.setAttribute('id', icons.typeName[i]);
+    typeIcon.setAttribute('class', 'listBtn');
+    navTypes.appendChild(typeIcon);
+  }
 }
-document.getElementById('pokeType').addEventListener('click', showByType);
+document.getElementById('pokeType').addEventListener('click', showTypeList);
 
-let showByEgg = () => {
+let showEggList = () => {
   document.getElementById('egg').style.display = 'flex';
   document.getElementById('types').style.display = 'none';
+  navEggs.textContent = '';
+  for (let i=0; i < icons.eggs.length; i++) {
+    const eggBtn = document.createElement('a');
+    const eggIcon = document.createElement('img');
+    const eggText = document.createElement('p');
+    eggBtn.appendChild(eggIcon);
+    eggBtn.appendChild(eggText);
+    eggIcon.src = icons.eggs[i];
+    eggText.innerHTML = icons.eggKm[i] + ' Kilometros';
+    eggBtn.setAttribute('id', icons.eggKm[i]);
+    eggBtn.setAttribute('class', 'icon');
+    eggIcon.setAttribute('class', 'eggBtn');
+    eggText.setAttribute('class', 'subtitle');
+    navEggs.appendChild(eggBtn);
+  }
 }
-document.getElementById('eggKm').addEventListener('click', showByEgg);
+document.getElementById('eggKm').addEventListener('click', showEggList);
 
+const table = document.querySelector('#pokeList');
+let selected;
+let tableFunction = (event) => {
+  let item = event.target.closest('td');
+  let chosenPoke = item.id;
+  console.log(chosenPoke);
+  if (!item) return;
+  if (!table.contains(item)) return;
+  highlight(item);
+};
+let highlight = (node) => {
+  if (selected) {
+    selected.classList.remove('highlight');
+  }
+  selected = node;
+  selected.classList.add('highlight');
+}
+document.getElementById('pokeList').addEventListener('click', tableFunction);
 let restart = () => {
   document.getElementById('egg').style.display = 'none';
   document.getElementById('types').style.display = 'none';
